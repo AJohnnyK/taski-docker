ffacd0b324b4dd2542472dfc506aa2354a828fb2
Anastasiya Kurnosova <AK.johnny@yandex.ru>
2023-06-10 06:10:48 +0300

    Add changes1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.28.107', '127.0.0.1', 'localhost', 'thetaski.ddns.net']
 
